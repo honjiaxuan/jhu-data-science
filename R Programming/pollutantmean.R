0ec51f03e333f2a42502455ba73e9c57dce89bd0
honjiaxuan <honjiaxuan@users.noreply.github.com>
2017-02-05 19:23:10 +0800

    Update pollutantmean.R

--- R Programming/pollutantmean.R
+++ R Programming/pollutantmean.R
@@ -1,4 +1,16 @@
 pollutantmean <- function (directory,pollutant, ID = 1:332) {
+ ## 'directory' is a character vector of length 1 indicating 
+ ## the location of the CSV files
+ 
+ ## 'pollutant' is a character vector of length 1 indicating
+ ## the name of the pollutant for which we will calculate the mean;
+ ## either "sulfate" or "nitrate" in this assignment
+ 
+ ##'id' is an integer vector indicating the monitor ID numbers to be used
+ 
+ ## Return the mean of the pollutant across all monitors list
+ ## in the 'id' vector (ignoring NA values)
+ ## the results are rounded to 3 decimal places.
  files_list <- list.files (directory,full.names = TRUE)
  dat <- data.frame()
  for (i in ID) {
