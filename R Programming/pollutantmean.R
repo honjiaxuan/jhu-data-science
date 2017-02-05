1a5bae64c26019fdc33647dfb47013d28df26342
honjiaxuan <honjiaxuan@users.noreply.github.com>
2017-02-05 23:16:28 +0800

    Update pollutantmean.R

--- R Programming/pollutantmean.R
+++ R Programming/pollutantmean.R
@@ -11,6 +11,14 @@ pollutantmean <- function (directory,pollutant, ID = 1:332) {
  ## Return the mean of the pollutant across all monitors list
  ## in the 'id' vector (ignoring NA values)
  ## the results are rounded to 3 decimal places.
+ 
+ ## Write a function named 'pollutantmean' that calculates the mean of a pollutant 
+ ## (sulfate or nitrate) across a specified list of monitors. 
+ ## The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. 
+ ## Given a vector monitor ID numbers, 'pollutantmean' reads that monitors
+ ## ' particulate matter data from the directory specified in the 'directory' argument and returns the mean of the 
+ ## pollutant across all of the monitors, ignoring any missing values coded as NA.
+ 
  files_list <- list.files (directory,full.names = TRUE)
  dat <- data.frame()
  for (i in ID) {
