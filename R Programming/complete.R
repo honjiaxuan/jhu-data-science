58eb05246fb2b165322f12854d0cdb47d1439029
honjiaxuan <honjiaxuan@users.noreply.github.com>
2017-02-05 23:15:16 +0800

    Update complete.R

--- R Programming/complete.R
+++ R Programming/complete.R
@@ -13,6 +13,12 @@ complete <- function (directory, id = 1:332) {
 ## ...
 ## where 'id' is the monitor ID number and 'nobs' is the 
 ## number of complete cases
+  
+##  Write a function that reads a directory full of files and 
+##  reports the number of completely observed cases in each data file. 
+##  The function should return a data frame where the first column is the name 
+##  of the file and the second column is the number of complete cases. 
+  
 
 
 files_list <- list.files(directory, full.names = TRUE)
